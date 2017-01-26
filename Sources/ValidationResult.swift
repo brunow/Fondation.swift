87762556eb02259e7452709b9ceab5eef1e26cb0
Bruno Wernimont <bruno.wernimont@gmail.com>
2017-01-26 16:41:25 +0100

    ValidationResult

---
+++ Sources/ValidationResult.swift
@@ -0,0 +1,58 @@
+//
+//	Swift Foundation framework.
+//	Created by:				Bruno Wernimont
+//
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
+import Foundation
+
+// Inspired by RxSwift example
+enum ValidationResult {
+    case valid
+    case empty
+    case validating
+    case failed(message: String?)
+}
+
+extension ValidationResult: CustomStringConvertible {
+    
+    var isValid: Bool {
+        switch self {
+        case .valid:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .valid:
+            return "valid"
+        case .empty:
+            return "empty"
+        case .validating:
+            return "validating"
+        case let .failed(message):
+            return message ?? "failed"
+        }
+    }
+    
+}
